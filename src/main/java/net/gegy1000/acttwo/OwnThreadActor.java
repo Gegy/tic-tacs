9fb49d2c39830729a715559eed3cf45e218f0c38
gegy1000 <gegy1000@gmail.com>
2020-06-23 15:37:11 +0200

    merge the threads of actors by the same name

--- src/main/java/net/gegy1000/acttwo/OwnThreadActor.java
+++ src/main/java/net/gegy1000/acttwo/OwnThreadActor.java
@@ -6,26 +6,45 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public final class OwnThreadActor<T> extends TaskExecutor<T> {
     private static final Logger LOGGER = LogManager.getLogger(OwnThreadActor.class);
+    private static final Map<String, OwnThreadActor<Runnable>> ACTORS = new HashMap<>();
 
     private final Object lock = new Object();
 
+    private final AtomicInteger refCount = new AtomicInteger();
     private volatile boolean active = true;
 
     private OwnThreadActor(TaskQueue<? super T, ? extends Runnable> queue, String name) {
         super(queue, task -> {}, name);
-
-        Thread thread = new Thread(this);
-        thread.setName("actor-" + name);
-        thread.setDaemon(true);
-        thread.start();
     }
 
     public static OwnThreadActor<Runnable> create(String name) {
+        OwnThreadActor<Runnable> actor;
+        synchronized (ACTORS) {
+            // merge actors by the same name
+            actor = ACTORS.computeIfAbsent(name, OwnThreadActor::startActor);
+        }
+
+        actor.acquireRef();
+
+        return actor;
+    }
+
+    private static OwnThreadActor<Runnable> startActor(String name) {
         TaskQueue.Simple<Runnable> queue = new TaskQueue.Simple<>(new ArrayDeque<>());
-        return new OwnThreadActor<>(queue, name);
+        OwnThreadActor<Runnable> actor = new OwnThreadActor<>(queue, name);
+
+        Thread thread = new Thread(actor);
+        thread.setName(name + "-actor");
+        thread.setDaemon(true);
+        thread.start();
+
+        return actor;
     }
 
     @Override
@@ -55,13 +74,31 @@ public final class OwnThreadActor<T> extends TaskExecutor<T> {
         }
     }
 
+    private void acquireRef() {
+        this.refCount.getAndIncrement();
+    }
+
+    private boolean releaseRef() {
+        return this.refCount.decrementAndGet() <= 0;
+    }
+
+    private void stop() {
+        synchronized (this.lock) {
+            this.active = false;
+            this.lock.notify();
+        }
+
+        synchronized (ACTORS) {
+            ACTORS.remove(this.getName());
+        }
+    }
+
     @Override
     public void close() {
         super.close();
 
-        synchronized (this.lock) {
-            this.active = false;
-            this.lock.notify();
+        if (this.releaseRef()) {
+            this.stop();
         }
     }
 }
