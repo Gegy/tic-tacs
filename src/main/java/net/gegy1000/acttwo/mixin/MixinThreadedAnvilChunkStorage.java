95212bfc3c45971c8e4dfc5391e78b7a0b63c08b
gegy1000 <gegy1000@gmail.com>
2020-07-02 21:27:35 +0200

    remove unused code

--- src/main/java/net/gegy1000/acttwo/mixin/MixinThreadedAnvilChunkStorage.java
+++ src/main/java/net/gegy1000/acttwo/mixin/MixinThreadedAnvilChunkStorage.java
@@ -1,52 +1,33 @@
 package net.gegy1000.acttwo.mixin;
 
-import com.mojang.datafixers.DataFixer;
 import com.mojang.datafixers.util.Either;
 import net.gegy1000.acttwo.VoidActor;
 import net.gegy1000.acttwo.chunk.AsyncChunkState;
-import net.gegy1000.acttwo.chunk.worker.ChunkGenWorker;
 import net.gegy1000.acttwo.chunk.ChunkHolderExt;
 import net.gegy1000.acttwo.chunk.TacsExt;
 import net.gegy1000.acttwo.chunk.future.GetChunkContext;
 import net.gegy1000.acttwo.chunk.future.UnloadedChunk;
 import net.gegy1000.acttwo.chunk.future.UpgradeChunk;
 import net.gegy1000.acttwo.chunk.future.VanillaChunkFuture;
+import net.gegy1000.acttwo.chunk.worker.ChunkGenWorker;
 import net.gegy1000.justnow.future.Future;
-import net.minecraft.server.WorldGenerationProgressListener;
 import net.minecraft.server.world.ChunkHolder;
-import net.minecraft.server.world.ChunkTaskPrioritySystem;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.server.world.ThreadedAnvilChunkStorage;
-import net.minecraft.structure.StructureManager;
 import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.thread.MessageListener;
 import net.minecraft.util.thread.TaskExecutor;
-import net.minecraft.util.thread.ThreadExecutor;
-import net.minecraft.world.PersistentStateManager;
 import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.chunk.ChunkProvider;
 import net.minecraft.world.chunk.ChunkStatus;
-import net.minecraft.world.gen.chunk.ChunkGenerator;
-import net.minecraft.world.level.storage.LevelStorage;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
-import java.util.function.Supplier;
 
 @Mixin(ThreadedAnvilChunkStorage.class)
 public abstract class MixinThreadedAnvilChunkStorage implements TacsExt {
-    @Shadow
-    @Final
-    private MessageListener<ChunkTaskPrioritySystem.Task<Runnable>> worldGenExecutor;
-
     @Shadow
     protected abstract ChunkHolder getCurrentChunkHolder(long pos);
 
@@ -66,25 +47,6 @@ public abstract class MixinThreadedAnvilChunkStorage implements TacsExt {
     )
     private TaskExecutor<Runnable> createWorldgenActor(Executor executor, String name) {
         return VoidActor.INSTANCE;
-    }
-
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void init(
-            ServerWorld world,
-            LevelStorage.Session session,
-            DataFixer fixer,
-            StructureManager structureManager,
-            Executor workerExecutor,
-            ThreadExecutor<Runnable> mainThreadExecutor,
-            ChunkProvider chunkProvider,
-            ChunkGenerator chunkGenerator,
-            WorldGenerationProgressListener progressListener,
-            Supplier<PersistentStateManager> persistentStateSupplier,
-            int viewDistance,
-            boolean bl,
-            CallbackInfo ci
-    ) {
-        this.worldGenExecutor.close();
     }
 
     private ThreadedAnvilChunkStorage self() {
