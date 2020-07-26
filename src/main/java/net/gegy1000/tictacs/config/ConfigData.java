ad71e44049935c84f0e84ade265bbabf4f06f3d1
SuperCoder7979 <k.pranav@gmail.com>
2020-07-25 21:19:07 -0400

    thread count defaults to the amount of processors

--- src/main/java/net/gegy1000/tictacs/config/ConfigData.java
+++ src/main/java/net/gegy1000/tictacs/config/ConfigData.java
@@ -5,8 +5,8 @@ import net.gegy1000.tictacs.TicTacs;
 public class ConfigData {
 	public String version = TicTacs.VERSION;
 
+	public int threadCount = Runtime.getRuntime().availableProcessors();
 	public int featureGenerationRadius = 1;
-	public int threadCount = 4;
 
 	public boolean debugChunkTickets = false;
 }
