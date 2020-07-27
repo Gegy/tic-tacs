a188de899251d8606d10537499463c84db4a9a2e
gegy1000 <gegy1000@gmail.com>
2020-07-27 19:07:09 +0200

    increase chunk ticket throttling limit

---
+++ src/main/java/net/gegy1000/tictacs/mixin/MixinChunkTicketManager.java
@@ -0,0 +1,14 @@
+package net.gegy1000.tictacs.mixin;
+
+import net.minecraft.server.world.ChunkTicketManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ChunkTicketManager.class)
+public class MixinChunkTicketManager {
+    @ModifyConstant(method = "<init>", constant = @Constant(intValue = 4))
+    private int getMaxTasks(int maxTasks) {
+        return 128;
+    }
+}
