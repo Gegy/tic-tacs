8df336ad0ac8d2e65d36b6b2e67f90ddd08af732
gegy1000 <gegy1000@gmail.com>
2020-08-01 09:46:20 +0200

    remove max batch size for lighting processing

---
+++ src/main/java/net/gegy1000/tictacs/mixin/MixinServerLightingProvider.java
@@ -0,0 +1,77 @@
+package net.gegy1000.tictacs.mixin;
+
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectList;
+import net.minecraft.server.world.ChunkTaskPrioritySystem;
+import net.minecraft.server.world.ServerLightingProvider;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.thread.MessageListener;
+import net.minecraft.world.chunk.ChunkProvider;
+import net.minecraft.world.chunk.light.LightingProvider;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.function.IntSupplier;
+
+@Mixin(ServerLightingProvider.class)
+public abstract class MixinServerLightingProvider extends LightingProvider {
+    @Shadow
+    @Final
+    private MessageListener<ChunkTaskPrioritySystem.Task<Runnable>> executor;
+
+    private final ObjectList<Runnable> preUpdateQueue = new ObjectArrayList<>();
+    private final ObjectList<Runnable> postUpdateQueue = new ObjectArrayList<>();
+
+    private MixinServerLightingProvider(ChunkProvider chunks, boolean blockLight, boolean skyLight) {
+        super(chunks, blockLight, skyLight);
+    }
+
+    @Redirect(
+            method = "tick",
+            at = @At(value = "INVOKE", target = "Lit/unimi/dsi/fastutil/objects/ObjectList;isEmpty()Z")
+    )
+    private boolean isQueueEmpty(ObjectList<?> queue) {
+        return this.preUpdateQueue.isEmpty() && this.postUpdateQueue.isEmpty();
+    }
+
+    /**
+     * @reason Don't try run tasks when they are being enqueued: leave that to happen from the tick scheduler
+     * @author gegy1000
+     */
+    @Overwrite
+    private void enqueue(int x, int z, IntSupplier levelSupplier, ServerLightingProvider.Stage stage, Runnable task) {
+        ObjectList<Runnable> queue;
+        if (stage == ServerLightingProvider.Stage.PRE_UPDATE) {
+            queue = this.preUpdateQueue;
+        } else {
+            queue = this.postUpdateQueue;
+        }
+
+        this.executor.send(ChunkTaskPrioritySystem.createMessage(() -> {
+            queue.add(task);
+        }, ChunkPos.toLong(x, z), levelSupplier));
+    }
+
+    /**
+     * @reason Remove lighting batch size limit
+     * We don't have to worry about overloading the thread-pool because each actor is given its own OS thread.
+     * @author gegy1000
+     */
+    @Overwrite
+    private void runTasks() {
+        this.runQueue(this.preUpdateQueue);
+        super.doLightUpdates(Integer.MAX_VALUE, true, true);
+        this.runQueue(this.postUpdateQueue);
+    }
+
+    private void runQueue(ObjectList<Runnable> queue) {
+        for (Runnable task : queue) {
+            task.run();
+        }
+        queue.clear();
+    }
+}
