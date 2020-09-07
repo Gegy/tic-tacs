0629a7e0d43d9c3742a1a5c1b86bdac316f08b04
gegy1000 <gegy1000@gmail.com>
2020-09-07 20:44:27 +0200

    checkEntityChunkPos logic is now equivalent to vanilla

--- src/main/java/net/gegy1000/tictacs/mixin/unblocking/ServerWorldMixin.java
+++ src/main/java/net/gegy1000/tictacs/mixin/unblocking/ServerWorldMixin.java
@@ -5,13 +5,11 @@ import net.gegy1000.tictacs.NonBlockingWorldAccess;
 import net.gegy1000.tictacs.chunk.step.ChunkStep;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.entity.Entity;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.server.world.ServerChunkManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.profiler.Profiler;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.Heightmap;
@@ -22,7 +20,6 @@ import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.concurrent.CompletableFuture;
@@ -36,38 +33,6 @@ public abstract class ServerWorldMixin extends World implements NonBlockingWorld
 
     private ServerWorldMixin(MutableWorldProperties properties, RegistryKey<World> registryKey, DimensionType dimensionType, Supplier<Profiler> supplier, boolean client, boolean debugWorld, long biomeSeed) {
         super(properties, registryKey, dimensionType, supplier, client, debugWorld, biomeSeed);
-    }
-
-    /**
-     * @reason don't block to load chunk if it is not already loaded
-     * @author gegy1000
-     */
-    @Overwrite
-    public void checkEntityChunkPos(Entity entity) {
-        if (!entity.isChunkPosUpdateRequested()) {
-            return;
-        }
-
-        this.getProfiler().push("chunkCheck");
-
-        int chunkX = MathHelper.floor(entity.getX()) >> 4;
-        int chunkY = MathHelper.floor(entity.getY()) >> 4;
-        int chunkZ = MathHelper.floor(entity.getZ()) >> 4;
-
-        // "updateNeeded" is more "inChunk"
-        if (!entity.updateNeeded || entity.chunkX != chunkX || entity.chunkY != chunkY || entity.chunkZ != chunkZ) {
-            if (entity.updateNeeded && this.isChunkLoaded(entity.chunkX, entity.chunkZ)) {
-                this.getChunk(entity.chunkX, entity.chunkZ).remove(entity, entity.chunkY);
-            }
-
-            if (entity.teleportRequested() || this.isChunkLoaded(chunkX, chunkZ)) {
-                this.getChunk(chunkX, chunkZ).addEntity(entity);
-            } else {
-                entity.updateNeeded = false;
-            }
-        }
-
-        this.getProfiler().pop();
     }
 
     @Override
